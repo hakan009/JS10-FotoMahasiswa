92760a6e4186fb71179083ee169db512e0622030
Hakan Alif Pramudya <hakanalifpramudya13@gmail.com>
2023-05-09 16:05:37 +0700

    Tugas 1 Jobsheet 10

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routess
+| Web Routesss
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
